761ed247eac5438a7690d2d5b8e3599171b6b800
victorialbrock <35825804+victorialbrock@users.noreply.github.com>
2018-02-12 06:36:36 -0800

    Create art-deco.js
    hw3

---
+++ art-deco.js
@@ -0,0 +1,14 @@
+createCanvas(400, 300);
+background(255);
+strokeWeight(25);
+stroke(0);
+
+int (y = 200);
+while(y > 5) {
+int (x = 200);
+while(x > 5) {
+rect(150, 150, y, x);
+y = y - 20;
+x = x - 20;
+}
+}
