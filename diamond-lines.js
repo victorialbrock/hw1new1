beabef36e925168d699b025bd417602696f90c1b
victorialbrock <35825804+victorialbrock@users.noreply.github.com>
2018-02-12 06:35:16 -0800

    Create diamond-lines.js
    hw3

---
+++ diamond-lines.js
@@ -0,0 +1,49 @@
+
+createCanvas(300, 300);
+background(255);
+strokeWeight(1);
+stroke(0);
+
+var x = 0;
+line(75, x, 100, x);
+x = x + 10
+line(70, x, 105, x);
+x = x + 10
+line(65, x, 110, x);
+x = x + 10
+line(60, x, 115, x);
+x = x + 10
+line(55, x, 120, x);
+x = x + 10
+line(50, x, 125, x);
+x = x + 10
+line(45, x, 130, x);
+x = x + 10
+line(40, x, 135, x);
+x = x + 10
+line(35, x, 140, x);
+x = x + 10
+line(30, x, 145, x);
+x = x + 10
+line(25, x, 150, x);
+x = x + 10
+line(30, x, 145, x);
+x = x + 10
+line(35, x, 140, x);
+x = x + 10
+line(40, x, 135, x);
+x = x + 10
+line(45, x, 130, x);
+x = x + 10
+line(50, x, 125, x);
+x = x + 10
+line(55, x, 120, x);
+x = x + 10
+line(60, x, 115, x);
+x = x + 10
+line(65, x, 110, x);
+x = x + 10
+line(70, x, 105, x);
+x = x + 10
+line(75, x, 100, x);
+x = x + 10
