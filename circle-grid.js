50fed745229126ec5cf7da1fe7d5f82b66eefcc3
victorialbrock <35825804+victorialbrock@users.noreply.github.com>
2018-02-12 06:37:06 -0800

    Create circle-grid.js
    hw3

---
+++ circle-grid.js
@@ -0,0 +1,61 @@
+createCanvas(400,300)
+background(0);
+strokeWeight(2);
+stroke(255);
+
+
+int (x = 0);
+while(x < width) {
+  ellipse(x, 25, 25, 25);
+  x = x + 40;
+}
+ 
+int (x = 0);
+while(x < width) {
+  ellipse(x, 60, 25, 25);
+  x = x + 40;
+}
+
+int (x = 0);
+while(x < width) {
+  ellipse(x, 95, 25, 25);
+  x = x + 40;
+}
+
+
+int (x = 0);
+while(x < width) {
+  ellipse(x, 130, 25, 25);
+  x = x + 40;
+}
+
+int (x = 0);
+while(x < width) {
+  ellipse(x, 165, 25, 25);
+  x = x + 40;
+}
+
+int (x = 0);
+while(x < width) {
+  ellipse(x, 200, 25, 25);
+  x = x + 40;
+}
+
+int (x = 0);
+while(x < width) {
+  ellipse(x, 235, 25, 25);
+  x = x + 40;
+}
+
+int (x = 0);
+while(x < width) {
+  ellipse(x, 270, 25, 25);
+  x = x + 40;
+}
+
+int (x = 0);
+while(x < width) {
+  ellipse(x, 305, 25, 25);
+  x = x + 40;
+}
+
